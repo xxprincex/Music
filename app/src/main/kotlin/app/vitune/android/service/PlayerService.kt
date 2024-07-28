42cd05db0a1e216d6bea5f714fd4f1f1cfc061b0
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-07-28 15:26:55 +0200

    Add METADATA_KEY_ALBUM_ART and METADATA_KEY_ALBUM_ART_URI to media session metadata for compatibility (closes #396)

--- app/src/main/kotlin/app/vitune/android/service/PlayerService.kt
+++ app/src/main/kotlin/app/vitune/android/service/PlayerService.kt
@@ -675,17 +675,16 @@ class PlayerService : InvincibleService(), Player.Listener, PlaybackStatsListene
     private fun maybeShowSongCoverInLockScreen() = handler.post {
         val bitmap = if (isAtLeastAndroid13 || AppearancePreferences.isShowingThumbnailInLockscreen)
             bitmapProvider.bitmap else null
+        val uri = player.mediaMetadata.artworkUri?.toString()?.thumbnail(512)
 
         metadataBuilder.putBitmap(MediaMetadata.METADATA_KEY_ART, bitmap)
-        metadataBuilder.putString(
-            MediaMetadata.METADATA_KEY_ART_URI,
-            player.mediaMetadata.artworkUri?.toString()?.thumbnail(512)
-        )
+        metadataBuilder.putString(MediaMetadata.METADATA_KEY_ART_URI, uri)
 
-        if (isAtLeastAndroid13 && player.currentMediaItemIndex == 0) metadataBuilder.putText(
-            MediaMetadata.METADATA_KEY_TITLE,
-            "${player.mediaMetadata.title} "
-        )
+        metadataBuilder.putBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART, bitmap)
+        metadataBuilder.putString(MediaMetadata.METADATA_KEY_ALBUM_ART_URI, uri)
+
+        if (isAtLeastAndroid13 && player.currentMediaItemIndex == 0)
+            metadataBuilder.putText(MediaMetadata.METADATA_KEY_TITLE, "${player.mediaMetadata.title} ")
 
         mediaSession.setMetadata(metadataBuilder.build())
     }
