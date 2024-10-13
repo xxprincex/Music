7cf255ee6cbeeeb3cb21e5d114625eb1acab8f78
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-10-13 21:17:29 +0200

    Remove all custom parcelable implementations

--- app/src/main/kotlin/app/vitune/android/models/Mood.kt
+++ app/src/main/kotlin/app/vitune/android/models/Mood.kt
@@ -1,37 +1,18 @@
 package app.vitune.android.models
 
-import android.os.Parcel
 import android.os.Parcelable
 import androidx.compose.ui.graphics.Color
+import app.vitune.core.ui.ParcelableColor
 import app.vitune.providers.innertube.Innertube
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Mood(
     val name: String,
-    val color: Color,
+    val color: ParcelableColor,
     val browseId: String?,
     val params: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        name = parcel.readString()!!,
-        color = Color(parcel.readLong()),
-        browseId = parcel.readString()!!,
-        params = parcel.readString()!!
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) = with(parcel) {
-        writeString(name)
-        writeLong(color.value.toLong())
-        writeString(browseId)
-        writeString(params)
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Parcelable.Creator<Mood> {
-        override fun createFromParcel(parcel: Parcel) = Mood(parcel)
-        override fun newArray(size: Int) = arrayOfNulls<Mood>(size)
-    }
-}
+) : Parcelable
 
 fun Innertube.Mood.Item.toUiMood() = Mood(
     name = title,
