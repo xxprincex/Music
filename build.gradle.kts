d66e1694814f707d35bde5b9407643cb65bcab70
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2025-01-25 17:54:21 +0100

    Bump to v1.1.3 and add fastlane changelogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ val clean by tasks.registering(Delete::class) {
 
 allprojects {
     group = "app.vitune"
-    version = "1.1.2"
+    version = "1.1.3"
 
     apply(plugin = "io.gitlab.arturbosch.detekt")
 
